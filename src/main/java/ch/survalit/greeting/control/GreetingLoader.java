20c6105fadc22565c1994d7e3e3c303610a798fa
John Paulo Sarmiento <paulo@Johns-Air.lan>
2022-10-22 19:43:49 +0800

    template loader test

--- src/main/java/ch/survalit/greeting/control/GreetingLoader.java
+++ src/main/java/ch/survalit/greeting/control/GreetingLoader.java
@@ -12,6 +12,7 @@ import javax.json.JsonObject;
 
 import ch.survalit.greeting.entity.Address;
 import ch.survalit.greeting.entity.Greeting;
+import ch.survalit.greeting.entity.Student;
 
 @ApplicationScoped
 public class GreetingLoader {
@@ -84,5 +85,47 @@ public class GreetingLoader {
         String studentResult = "id: " + id + " ,fname: " + fname + " ,mname: " + mname + " ,lname: " + lname + " ,age: " + age + " ,birthday: " + bday + " ,inserted_date: " + inserted_date + " ,student no: " + student_no;
         return studentResult; 
         
+    }
+
+    public Student returnUserTemplate(int userID) {
+        Connection conDB=null;
+        int id = 0;
+        String fname = null;
+        String mname = null;
+        String lname = null;
+        int age = 0;
+        String bday = null;
+        String inserted_date = null;
+        int student_no = 0;
+
+        try{
+            conDB = DriverManager.getConnection("jdbc:mariadb://localhost:3306/student_db", "root", "");
+            
+            String sql = "SELECT * FROM student_info WHERE id = " + userID + ";";
+            Statement statement = conDB.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+
+            while (result.next()){
+                id = result.getInt("id");
+                fname = result.getString("fname");
+                mname = result.getString("mname");
+                lname = result.getString("lname");
+                age = result.getInt("age");
+                bday = result.getString("birthday");
+                inserted_date = result.getString("inserted_date");
+                student_no = result.getInt("student_no");
+            }
+
+            if (conDB != null) {
+                //System.out.println("Connected Successfully");
+            }
+        } catch (SQLException ex) {ex.printStackTrace();}
+        finally {
+            try{conDB.close();}
+            catch (Exception e) {
+                //do nothing
+            }
+        }
+        return new Student(id, fname, mname, lname, age, bday, inserted_date, student_no);
     }
 }

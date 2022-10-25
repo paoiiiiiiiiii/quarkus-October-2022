28b5efe07b1cc02dae189558062e4092e94d32c1
paoiiiiiiiiii <105591086+paoiiiiiiiiii@users.noreply.github.com>
2022-10-25 22:21:18 +0800

    student database

--- forest-service-db/init.sql
+++ forest-service-db/init.sql
@@ -1,1 +1,113 @@
 -- create table ....
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Oct 25, 2022 at 04:18 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sex`
+--
+
+CREATE TABLE `sex` (
+  `id` int(11) NOT NULL,
+  `student_no` int(11) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sex`
+--
+
+INSERT INTO `sex` (`id`, `student_no`, `gender`) VALUES
+(1, 202002010, 'Male'),
+(2, 20201293, 'Male'),
+(3, 202011348, 'Female'),
+(4, 202001174, 'Female'),
+(5, 202010200, 'Male');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_info`
+--
+
+CREATE TABLE `student_info` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(30) DEFAULT NULL,
+  `mname` varchar(30) DEFAULT NULL,
+  `lname` varchar(30) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `inserted_date` date DEFAULT NULL,
+  `student_no` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student_info`
+--
+
+INSERT INTO `student_info` (`id`, `fname`, `mname`, `lname`, `age`, `birthday`, `inserted_date`, `student_no`) VALUES
+(1, 'John Paulo', 'Rosario', 'Sarmiento', 21, '2001-10-10', '2022-10-12', 202002010),
+(2, 'Darren Jay', 'Pornelos', 'Salgado', 21, '2002-04-11', '2022-10-12', 20201293),
+(3, 'Ma. Collene', 'Palas', 'Francisco', 21, '2001-07-14', '2022-10-12', 202011348),
+(4, 'Francesca Marie', 'Mina', 'Besabe', 21, '2001-04-28', '2022-10-12', 202001174),
+(5, 'Fritz Jannuz', 'Viray', 'Aniciete', 20, '2002-01-02', '2022-10-12', 202010200);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `sex`
+--
+ALTER TABLE `sex`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `student_no` (`student_no`);
+
+--
+-- Indexes for table `student_info`
+--
+ALTER TABLE `student_info`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `student_no` (`student_no`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `sex`
+--
+ALTER TABLE `sex`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `student_info`
+--
+ALTER TABLE `student_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
